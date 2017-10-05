21a73474193fe2bbe4f7ba7058da1c145e3144ca
yangchangseok <32148670+yangchangseok@users.noreply.github.com>
2017-10-05 18:25:50 +0900

    Add files via upload

--- src/commands.c
+++ src/commands.c
@@ -2,29 +2,42 @@
 
 #include "commands.h"
 
-int do_cd(int argc, char** argv) {
-  if (!validate_cd_argv(argc, argv))
-    return -1;
+#include <stdio.h>		// for printf
+#include <unistd.h>		// for linux system call
+#define BUFF_SIZE 512	// size of buffer for getcwd
 
-  // TODO: Fill it!
-  return 0;
+int do_cd(int argc, char** argv) {
+	if (!validate_cd_argv(argc, argv))
+    	return -1;
+
+	if(chdir(argv[1])== 0)	return 0;	// success to system call : chdir 
+	else					return -1;	// error in chdir, can check the error number using errno.h's errno
 }
 
 int do_pwd(int argc, char** argv) {
-  if (!validate_pwd_argv(argc, argv))
-    return -1;
+	if (!validate_pwd_argv(argc, argv))
+    	return -1;
 
-  // TODO: Fill it!
+	// declare and initialize the buffer
+	// for path from getcwd system call
+	char buff[BUFF_SIZE] = {0, };
 
-  return 0;
+	if(getcwd(buff, 512) == NULL) {
+		// error in getcwd, can check the error number using errno.h's errno
+		return -1;
+	} else {
+		// success to system call : getcwd
+		printf("%s\n", buff);
+		return 0;
+	}
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	// mysh's cd can only has two argv(1:cd, 2:path)
+  	return (argc == 2) && (strcmp(argv[0], "cd") == 0) ? 1 : 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+	// mysh's pwd can only has one argv(1:pwd)
+	return (argc == 1) && (strcmp(argv[0], "pwd") == 0) ? 1 : 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,61 @@
 #include "utils.h"
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
-{
-  // TODO: Fill this!
+#include <memory.h>	// for malloc and realloc
+#include <stdlib.h>	// str* methods
+#include <string.h>	// for strtok
+#include <stdio.h>	// for NULL
+
+void mysh_parse_command(
+		const char* command,
+        int *argc,
+		char*** argv) {
+
+	// reset number of arguments to 0
+	(*argc) = 0;
+
+	// get length of command, and add 1. last 1 is the space for '\0'
+	const int command_length = strlen(command) + 1;	
+
+	// allocate the memory space for copy of command
+	// cause method:strtok's first parameter shoud be char*
+	// but mysh_parse_command's first parameter is const char*
+	char* copied_command = (char*) malloc(command_length * sizeof(char));
+
+	// copy the command 
+	strncpy(copied_command, command, command_length);
+	
+	// split the string using delimeter space, tab and newline
+	const char* delim = " \t\n";
+
+	// STRTOK -> take full string to be tokenized at first call
+	char* token = strtok(copied_command, delim);
+
+	// return NULL from the strtok at first call
+	// mean the string is empty or all member of string are delim
+
+	// so if strtok's first call return NULL, just make array of argv and push empty string:""
+	// else read and copy the tokenized arg one by one
+	do {
+
+		// grow an array dinamically
+		// if first argument, just malloc
+		// else re-allocate the argv array
+		switch (*argc) {
+			case 0:		*argv = (char**) malloc(sizeof(char*));							break;
+			default:	*argv = (char**) realloc(*argv, ((*argc) + 1) * sizeof(char*));	break;
+		}
+
+		// allocate the memory and copy the argument
+		// then point the growed argv array
+		strcpy(
+			*((*argv) + (*argc)++) = (char*) malloc(token == NULL ? 1 : strlen(token)),
+			token == NULL ? "" : token);
+
+	// STRTOK -> from the second time, strtok take NULL
+	// end if there is no more token, return zero
+	// let's loop untill strtok return NULL...
+	} while((token = strtok(NULL, delim)) != NULL);
+
+	// return the allocated memory
+	free(copied_command);
 }
